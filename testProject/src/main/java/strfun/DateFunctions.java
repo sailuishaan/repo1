e98818d8a3c6e2001134d20d900f9faaedbbb106
Sowji <Sowji@Sowji-PC>
2016-08-17 22:16:20 +1000

    commit first version

---
+++ testProject/src/main/java/strfun/DateFunctions.java
@@ -0,0 +1,18 @@
+package strfun;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFunctions {
+
+	public static void main(String[] args) {
+		Date currentdate=new Date();
+		System.out.println(currentdate);
+		DateFormat format=new SimpleDateFormat("dd/MM/YYYY hh:mm:ss:SSS");
+		String datestring=format.format(currentdate);
+		System.out.println(datestring);
+
+	}
+
+}
